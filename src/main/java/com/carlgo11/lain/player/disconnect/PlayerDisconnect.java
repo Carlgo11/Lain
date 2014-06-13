acdfd31dee7dbd06f30a2cfee9bedd09b92011cc
Carlgo11 <carlgo11@carlgo11.com>
2014-06-13 18:30:24 +0200

    trying to fix notifications not being sent

--- src/main/java/com/carlgo11/lain/player/disconnect/PlayerDisconnect.java
+++ src/main/java/com/carlgo11/lain/player/disconnect/PlayerDisconnect.java
@@ -2,9 +2,10 @@ package com.carlgo11.lain.player.disconnect;
 
 import com.carlgo11.lain.Lain;
 import com.carlgo11.lain.Messages;
-import com.carlgo11.lain.Prowl;
 import com.carlgo11.lain.player.chat.commands.PerformCommand;
 import java.io.IOException;
+import java.net.URL;
+import java.net.URLConnection;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import org.bukkit.Bukkit;
@@ -39,17 +40,14 @@ public class PlayerDisconnect implements Listener {
                 Bukkit.dispatchCommand(Bukkit.getConsoleSender(), "stop");
             } else {
                 try {
-                    Prowl.send("Server rebooted", 1);
+                    URL ipaddress = new URL("https://api.prowlapp.com/publicapi/add?apikey=be4a291fdbc93e5f54b35d95c16963bf245a20ee&priority=" + 1 + "&application=ping&event=" + "Server%20rebooted.");
+                    URLConnection ip = ipaddress.openConnection();
+                    ip.setDoOutput(true);
+                    ip.connect();
+                    Bukkit.dispatchCommand(Bukkit.getConsoleSender(), "stop");
                 } catch (IOException ex) {
                     Logger.getLogger(PerformCommand.class.getName()).log(Level.SEVERE, null, ex);
                 }
-                Bukkit.getScheduler().scheduleSyncDelayedTask(plugin, new Runnable() {
-
-                    public void run()
-                    {
-                        Bukkit.dispatchCommand(Bukkit.getConsoleSender(), "stop");
-                    }
-                }, 60l);
             }
         }
     }
