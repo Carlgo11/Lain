752e4751ec66f6bfa15058b2830b916189e6d070
Carlgo11 <carlgo11@carlgo11.com>
2014-06-15 21:18:11 +0200

    Starting to move dot commands from config to commands file

---
+++ src/main/java/com/carlgo11/lain/DotCommands.java
@@ -0,0 +1,27 @@
+package com.carlgo11.lain;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Properties;
+
+public class DotCommands {
+
+    private Lain Lain;
+    Properties file = new Properties();
+
+    public void main(Lain l) throws IOException
+    {
+        this.Lain = l;
+        file.load(new FileInputStream("messages.properties"));
+    }
+
+    public String getMessage(String command)
+    {
+        if (file.contains(command)) {
+            return file.getProperty(command);
+        }
+
+        return null;
+    }
+
+}
--- src/main/java/com/carlgo11/lain/player/chat/commands/ExternalCommands.java
+++ src/main/java/com/carlgo11/lain/player/chat/commands/ExternalCommands.java
@@ -18,45 +18,45 @@ public class ExternalCommands {
         String aliasbuild = "alias-" + argsnodot + "-end";
         String cmdprefix = ".";
         if (args[0].startsWith(cmdprefix)) {
-                if (args.length == 1) {
+            if (args.length == 1) {
+                if (plugin.getConfig().contains(cmdbuild)) {
+                    plugin.broadcastMessage(plugin.getConfig().getString(cmdbuild));
+                } else {
+                    if (plugin.getConfig().contains(aliasbuild)) {
+                        if (plugin.getConfig().contains(plugin.getConfig().getString(aliasbuild))) {
+                            plugin.broadcastMessage(plugin.getConfig().getString(plugin.getConfig().getString(aliasbuild))); // INCEPTION! :O
+                        }
+                    }
+                }
+
+            } else if (args.length > 1) {
+                if (Bukkit.getOfflinePlayer(args[1]).isOnline()) {
                     if (plugin.getConfig().contains(cmdbuild)) {
-                        plugin.broadcastMessage(plugin.getConfig().getString(cmdbuild));
+                        plugin.broadcastMessage(ChatColor.DARK_AQUA + args[1] + ": " + plugin.getConfig().getString(cmdbuild));
+                        Bukkit.getPlayer(args[1]).getWorld().playSound(Bukkit.getPlayer(args[1]).getLocation(), Sound.ORB_PICKUP, 1, 0);
                     } else {
                         if (plugin.getConfig().contains(aliasbuild)) {
                             if (plugin.getConfig().contains(plugin.getConfig().getString(aliasbuild))) {
-                                plugin.broadcastMessage(plugin.getConfig().getString(plugin.getConfig().getString(aliasbuild))); // INCEPTION! :O
+                                plugin.broadcastMessage(ChatColor.DARK_AQUA + args[1] + ": " + plugin.getConfig().getString(plugin.getConfig().getString(aliasbuild)));
+                                Bukkit.getPlayer(args[1]).getWorld().playSound(Bukkit.getPlayer(args[1]).getLocation(), Sound.ORB_PICKUP, 1, 0);
                             }
                         }
                     }
 
-                } else if (args.length > 1) {
-                    if (Bukkit.getOfflinePlayer(args[1]).isOnline()) {
+                } else {
+                    if (plugin.getConfig().contains(cmdbuild)) {
                         if (plugin.getConfig().contains(cmdbuild)) {
-                            plugin.broadcastMessage(ChatColor.DARK_AQUA + args[1] + ": " + plugin.getConfig().getString(cmdbuild));
-                            Bukkit.getPlayer(args[1]).getWorld().playSound(Bukkit.getPlayer(args[1]).getLocation(), Sound.ORB_PICKUP, 1, 0);
+                            plugin.broadcastMessage(plugin.getConfig().getString(cmdbuild));
                         } else {
                             if (plugin.getConfig().contains(aliasbuild)) {
                                 if (plugin.getConfig().contains(plugin.getConfig().getString(aliasbuild))) {
-                                    plugin.broadcastMessage(ChatColor.DARK_AQUA + args[1] + ": " + plugin.getConfig().getString(plugin.getConfig().getString(aliasbuild)));
-                                    Bukkit.getPlayer(args[1]).getWorld().playSound(Bukkit.getPlayer(args[1]).getLocation(), Sound.ORB_PICKUP, 1, 0);
-                                }
-                            }
-                        }
-
-                    } else {
-                        if (plugin.getConfig().contains(cmdbuild)) {
-                            if (plugin.getConfig().contains(cmdbuild)) {
-                                plugin.broadcastMessage(plugin.getConfig().getString(cmdbuild));
-                            } else {
-                                if (plugin.getConfig().contains(aliasbuild)) {
-                                    if (plugin.getConfig().contains(plugin.getConfig().getString(aliasbuild))) {
-                                        plugin.broadcastMessage(plugin.getConfig().getString(plugin.getConfig().getString(aliasbuild))); // INCEPTION! :O
-                                    }
+                                    plugin.broadcastMessage(plugin.getConfig().getString(plugin.getConfig().getString(aliasbuild))); // INCEPTION! :O
                                 }
                             }
                         }
                     }
                 }
+            }
         }
     }
 }
