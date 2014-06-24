3721c432de35fa5fcf90efe53f00365d9db9fcf6
Carlgo11 <carlgo11@carlgo11.com>
2014-06-24 22:31:59 +0200

    part 2 of last commit

--- src/main/java/com/carlgo11/lain/player/join/PlayerJoin.java
+++ src/main/java/com/carlgo11/lain/player/join/PlayerJoin.java
@@ -1,8 +1,10 @@
 package com.carlgo11.lain.player.join;
 
 import com.carlgo11.lain.Lain;
+import com.carlgo11.lain.Mysql;
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
@@ -19,6 +21,11 @@ public class PlayerJoin implements Listener {
     @EventHandler
     public void onPlayerJoin(PlayerJoinEvent event)
     {
+        Player p = event.getPlayer();
+        String rank = Mysql.getRank(p.getName());
+        if(rank != null){
+            Bukkit.dispatchCommand(Bukkit.getConsoleSender(), "manuadd "+p.getName()+" "+rank);
+        }
         int a = 0;
         int b = 1;
         for (int i = 1; a != b; i++) {
