626725dc8d3527b68a9cc1f762f28a226bdbd5e2
Carlgo11 <carlgo11@carlgo11.com>
2014-07-01 15:45:44 +0200

    cleanup

--- src/main/java/com/carlgo11/lain/ChatCommands.java
+++ src/main/java/com/carlgo11/lain/ChatCommands.java
@@ -1,17 +1,7 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 package com.carlgo11.lain;
 
 import org.bukkit.entity.Player;
 
-/**
- *
- * @author Carl
- */
 public interface ChatCommands {
     
     public String getCommandName();
