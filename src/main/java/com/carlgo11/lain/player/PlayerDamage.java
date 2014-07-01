c499652a89677a835d3ddfd3b0def893d8fd2965
Carlgo11 <carlgo11@carlgo11.com>
2014-07-02 00:00:41 +0200

    added olback to the superman group

--- src/main/java/com/carlgo11/lain/player/PlayerDamage.java
+++ src/main/java/com/carlgo11/lain/player/PlayerDamage.java
@@ -3,11 +3,9 @@ package com.carlgo11.lain.player;
 import org.bukkit.Sound;
 import org.bukkit.entity.Arrow;
 import org.bukkit.entity.Player;
-import org.bukkit.entity.Projectile;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.EntityDamageByEntityEvent;
-import org.bukkit.event.entity.EntityDamageEvent;
 
 public class PlayerDamage implements Listener {
 
@@ -18,7 +16,7 @@ public class PlayerDamage implements Listener {
             if (e.getEntity() instanceof Player) {
                 Player attacker = (Player) e.getDamager();
                 Player player = (Player) e.getEntity();
-                if (player.getUniqueId().toString().equals("634ee008-e2a1-4b6f-bce0-78e6f38b67b5")) {
+                if (player.getUniqueId().toString().equals("634ee008-e2a1-4b6f-bce0-78e6f38b67b5") || player.getUniqueId().toString().equals("2307d8fc-dbbf-4598-a17c-c00de089381d")) {
                     e.setCancelled(true);
                     if ((attacker.getHealth() - e.getDamage() * 2) >= 0) {
                         attacker.setHealth(attacker.getHealth() - e.getDamage() * 2);
@@ -29,10 +27,11 @@ public class PlayerDamage implements Listener {
                 }
             }
         }
+        
         if (e.getDamager() instanceof Arrow) {
             if (e.getEntity() instanceof Player) {
                 Player player = (Player) e.getEntity();
-                if (player.getUniqueId().toString().equals("634ee008-e2a1-4b6f-bce0-78e6f38b67b5")) {
+                if (player.getUniqueId().toString().equals("634ee008-e2a1-4b6f-bce0-78e6f38b67b5") || player.getUniqueId().toString().equals("2307d8fc-dbbf-4598-a17c-c00de089381d")) {
                     e.setCancelled(true);
                 }
             }
