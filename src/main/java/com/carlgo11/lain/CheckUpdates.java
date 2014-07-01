d210c40f1ef3c55e1439e5c72cf9aec8688ee006
Carlgo11 <carlgo11@carlgo11.com>
2014-07-01 14:34:19 +0200

    cleanup of CheckUpdates

--- src/main/java/com/carlgo11/lain/CheckUpdates.java
+++ src/main/java/com/carlgo11/lain/CheckUpdates.java
@@ -1,14 +1,12 @@
 package com.carlgo11.lain;
 
 import com.carlgo11.lain.player.disconnect.PlayerDisconnect;
-import java.io.File;
 import java.net.URL;
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.w3c.dom.Document;
-
 
 public class CheckUpdates {
 
@@ -18,27 +16,27 @@ public class CheckUpdates {
 
             public void run()
             {
-                try{
-        DocumentBuilderFactory documentBuilderFactory = DocumentBuilderFactory
-                .newInstance();
-        DocumentBuilder documentBuilder = documentBuilderFactory.newDocumentBuilder();
-        Document document = documentBuilder.parse(new URL("http://ci.carlgo11.com/rssLatest").openStream());
-        String v = document.getElementsByTagName("title").item(1).getTextContent();
-        String[] a = v.split(" ");
-        String xv = a[1].toString().replace("#", "");
-        String version = CheckUpdates.class.getPackage().getImplementationVersion();
-        if (version != "") {
-            System.out.println(xv+"\t"+version);
-            if (Integer.parseInt(xv) > Integer.parseInt(version)) {
-                Lain.broadcastMessage(ChatColor.GREEN + "New update found! Server restart scheduled.");
-                PlayerDisconnect.reboot = true;
-            }
-        }
-            }catch(Exception ex){
-              ex.printStackTrace();
-            }
+                try {
+                    DocumentBuilderFactory documentBuilderFactory = DocumentBuilderFactory
+                            .newInstance();
+                    DocumentBuilder documentBuilder = documentBuilderFactory.newDocumentBuilder();
+                    Document document = documentBuilder.parse(new URL("http://ci.carlgo11.com/rssLatest").openStream());
+                    String v = document.getElementsByTagName("title").item(1).getTextContent();
+                    String[] a = v.split(" ");
+                    String xv = a[1].toString().replace("#", "");
+                    String version = CheckUpdates.class.getPackage().getImplementationVersion();
+                    if (version != "") {
+                        System.out.println(xv + "\t" + version);
+                        if (Integer.parseInt(xv) > Integer.parseInt(version)) {
+                            Lain.broadcastMessage(ChatColor.GREEN + "New update found! Server restart scheduled.");
+                            PlayerDisconnect.reboot = true;
+                        }
+                    }
+                } catch (Exception ex) {
+                    ex.printStackTrace();
+                }
             }
         }, 20l, 401);
-        
+
     }
 }
