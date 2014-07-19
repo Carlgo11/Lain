7c1ede11b48db3259491baaee37010a99b31a580
Carlgo11 <carlgo11@carlgo11.com>
2014-07-19 21:03:49 +0200

    I derped

--- src/main/java/com/carlgo11/lain/player/chat/commands/DefineCommand.java
+++ src/main/java/com/carlgo11/lain/player/chat/commands/DefineCommand.java
@@ -24,10 +24,7 @@ public class DefineCommand implements ChatCommands {
                 ArrayList<String> a = WASearch.search(d.toString());
                 
                 for(int i = 0; i < a.size(); i++){
-                    if(a.get(i).equals("")){
                      Bukkit.broadcastMessage("");
-                }
-                
                 }
     }
 
